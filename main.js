c789bef7169c24bec67838297b14b1c0a8c047de
sandeepchhapola <schhapola0001@gmail.com>
2014-07-10 17:23:29 +0530

    changes applied

--- main.js
+++ main.js
@@ -1,20 +1,64 @@
 var firstApp = angular.module("firstApp", []);
 
-firstApp.controller('taskController',function($scope){
-    $scope.task= [];
-    $scope.addTask=function(data) {
-        if ((0 !== data.length)||(" "===data)) {
+firstApp.controller('taskController', function ($scope, $window) {
+    $scope.task = [];
+    $scope.tsk = {};
+    $scope.addTask = function (titl, desc) {
+        if ((0 !== titl.length) || (" " === titl)) {
             var cur_date = new Date();
             $scope.task.push({
-                name: data,
-                date: cur_date.getDay() + "/" + (cur_date.getMonth() + 1) + "/" + cur_date.getFullYear()
+                title: titl,
+                description: desc,
+                time: cur_date.getHours() + ':' + cur_date.getMinutes(),
+                date: cur_date.getDate() + "/" + (cur_date.getMonth() + 1) + "/" + cur_date.getFullYear()
             });
+            if (jQuery("#collapseme").hasClass("out")) {
+                jQuery("#collapseme").addClass("in");
+                jQuery("#collapseme").removeClass("out");
+            }
+            jQuery("#jobTitle").val("");
+            jQuery("#jobDesc").val("");
         }
         else
-            alert("empty value");
+            $window.alert("empty value");
     };
 
-    $scope.removeTask = function(index){
+    $scope.removeTask = function (index) {
         $scope.task.splice(index, 1);
+
+        if (jQuery.isEmptyObject($scope.task)) {
+            jQuery("#collapseme").addClass("out");
+            jQuery("#collapseme").removeClass("in");
+        }
     };
+
+    $scope.addTaskModal = function () {
+        jQuery('#getTaskModal').modal({
+            keyboard: true
+        });
+    };
+    $scope.dispalayTaskModal = function (index) {
+        $scope.tsk = $scope.task[index];
+        jQuery('#showTaskModal').modal({
+            keyboard: true
+        });
+    };
+
+});
+
+
+firstApp.directive("mouseon", function () {
+    return function ($scope, element, attrs) {
+        element.bind("mouseenter", function () {
+            element.addClass(attrs.mouseon)
+        });
+    }
+});
+
+firstApp.directive("mouseoff", function () {
+    return function ($scope, element, attrs) {
+        element.bind("mouseleave", function () {
+            element.removeClass(attrs.mouseon);
+        });
+    }
 });
